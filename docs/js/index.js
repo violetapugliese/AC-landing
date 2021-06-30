2fa4cf088ab3b39d2650e3d7d70edfcccb495fa8
Violeta Pugliese <violeta.pugliese@gmail.com>
2021-06-30 19:01:11 -0300

    header

---
+++ docs/js/index.js
@@ -0,0 +1,11 @@
+const menuBtn = document.getElementById('menu-btn');
+const navBar = document.getElementById('wrapper');
+const bar1 = document.getElementById('bar1');
+const bar2 = document.getElementById('bar2');
+const bar2 = document.getElementById('bar3');
+
+menuBtn.addEventListener('click', activeMenu)
+
+function activeMenu {
+    console.log('hola')
+}
