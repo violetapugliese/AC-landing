a3c492f589ae76a9ce3cbb480ef22ad5f1f0200b
Violeta Pugliese <violeta.pugliese@gmail.com>
2021-07-01 21:26:59 -0300

    preloader

--- docs/js/index.js
+++ docs/js/index.js
@@ -1,11 +1,4 @@
-const menuBtn = document.getElementById('menu-btn');
-const navBar = document.getElementById('wrapper');
-const bar1 = document.getElementById('bar1');
-const bar2 = document.getElementById('bar2');
-const bar2 = document.getElementById('bar3');
-
-menuBtn.addEventListener('click', activeMenu)
-
-function activeMenu {
-    console.log('hola')
+window.onload = function(){
+    $('#preloader').fadeOut();
+    $('body').removeClass('hidden')
 }
