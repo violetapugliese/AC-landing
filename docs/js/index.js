805dd80363bf8c3e75a1bff32191e03b6450c86b
Violeta Pugliese <violeta.pugliese@gmail.com>
2021-07-13 18:52:17 -0300

    working seo

--- docs/js/index.js
+++ docs/js/index.js
@@ -4,7 +4,6 @@ window.onload = function () {
 };
 
 var myNav = document.getElementById("nav-bar");
-var myBtn = document.getElementById("menu-btn-span");
 
 window.onscroll = function () {
   "use strict";
@@ -14,17 +13,13 @@ window.onscroll = function () {
     mirror: true,
   });
   if (
-    document.body.scrollTop >= 500 ||
-    document.documentElement.scrollTop >= 500
+    document.body.scrollTop >= 300 ||
+    document.documentElement.scrollTop >= 300
   ) {
     myNav.classList.add("nav-colored");
     myNav.classList.remove("nav-transparent");
-    myBtn.classList.add("btn-blue");
-    myBtn.classList.remove("btn-white");
   } else {
     myNav.classList.add("nav-transparent");
     myNav.classList.remove("nav-colored");
-    myBtn.classList.add("btn-white");
-    myBtn.classList.remove("btn-blue");
   }
 };
