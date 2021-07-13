cd9c5d83a02a27b0a83168a386fcbd6c900d23ce
Violeta Pugliese <violeta.pugliese@gmail.com>
2021-07-13 20:22:50 -0300

    working seo

--- docs/js/index.js
+++ docs/js/index.js
@@ -5,6 +5,36 @@ window.onload = function () {
 
 var myNav = document.getElementById("nav-bar");
 
+
+var navbar = document.querySelector('.wrapper')
+var ham = document.querySelector('.menu-btn')
+var ham1 = document.querySelector('.menu-btn-ham1')
+var ham2 = document.querySelector('.menu-btn-ham2')
+var ham3 = document.querySelector('.menu-btn-ham3')
+var menuContainer = document.querySelector('.nav-bar-menu-container')
+var menuLink = document.querySelectorAll(".nav-bar-menu-link")
+var manifestContainer = document.querySelector('.manifest-text-container')
+var btnManifest = document.querySelector('.btn-manifest')
+
+ham.addEventListener("click", toggleHamburger)
+function toggleHamburger() {
+    navbar.classList.toggle("showNav")
+    ham1.classList.toggle("closeBtn1")
+    ham2.classList.toggle("closeBtn2")
+    ham3.classList.toggle("closeBtn3")
+    menuContainer.classList.toggle("showMenuLink")
+}
+menuLink.forEach(
+    function (menuLink) {
+        menuLink.addEventListener("click", toggleHamburger)
+    }
+)
+
+btnManifest.addEventListener("click", expandContainer)
+function expandContainer() {
+    manifestContainer.classList.toggle("manifestExpand")
+}
+
 window.onscroll = function () {
   "use strict";
   AOS.init({
