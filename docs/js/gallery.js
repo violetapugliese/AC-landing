b4f40b632ec8a89e2d290d15958d05a5d38d93a0
Violeta Pugliese <violeta.pugliese@gmail.com>
2021-08-23 17:57:00 -0300

    new gallery

--- docs/js/gallery.js
+++ docs/js/gallery.js
@@ -1,25 +1,62 @@
-$('.gallery__slider').slick({
-    centerMode: true,
-    centerPadding: '60px',
-    slidesToShow: 3,
-    responsive: [
-        {
-            breakpoint: 768,
-            settings: {
-                arrows: false,
-                centerMode: true,
-                centerPadding: '40px',
-                slidesToShow: 3
-            }
-        },
-        {
-            breakpoint: 480,
-            settings: {
-                arrows: false,
-                centerMode: true,
-                centerPadding: '40px',
-                slidesToShow: 1
-            }
-        }
-    ]
+let slider = document.querySelector('.slider');
+let innerSlider = document.querySelector('.slider-inner');
+
+let pressed = false;
+let startx;
+let x;
+
+// Cursor 
+slider.addEventListener('mousedown', (e)=>{
+    pressed = true;
+    startx = e.offsetX - innerSlider.offsetLeft;
+    slider.style.cursor = 'grabbing'
 });
+slider.addEventListener('mousenter', ()=> {
+    slider.style.cursor = 'grab'
+});
+slider.addEventListener('mouseup', ()=>{
+    slider.style.cursor = 'grab';
+});
+window.addEventListener('mouseup', ()=>{
+    pressed = false;
+});
+// mouse event
+slider.addEventListener('mousemove', (e)=> {
+    if(!pressed) return;
+    e.preventDefault();
+
+    x = e.offsetX
+
+    innerSlider.style.left = `${x -startx}px`
+    
+    checkboundary()
+});
+// touch event 
+slider.addEventListener('touchstart', (e)=>{
+    pressed = true;
+    startx = e.touches[0].clientX - innerSlider.offsetLeft;
+ });
+ 
+ window.addEventListener('touchend', ()=>{
+    pressed = false;
+ });
+ slider.addEventListener('touchmove', (e)=>{
+    if(!pressed) return
+    e.preventDefault();
+ 
+    x = e.touches[0].clientX;
+ 
+    innerSlider.style.left = `${x - startx}px`;
+ 
+    checkboundary();
+ });
+
+function checkboundary(){
+    let outer = slider.getBoundingClientRect();
+    let inner = innerSlider.getBoundingClientRect();
+
+    if(parseInt(innerSlider.style.left) > 0){
+        innerSlider.style.left = '0px';
+    }else if(inner.right < outer.right){
+        innerSlider.style.left = `-${inner.width - outer.width}px`    }
+}
