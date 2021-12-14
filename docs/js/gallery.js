b14a0125dd2dd9ae26486a0a0a68e2f8b64f412d
violetapugliese <violeta.pugliese@gmail.com>
2021-12-14 20:42:10 -0300

    resize gallery images

--- docs/js/gallery.js
+++ docs/js/gallery.js
@@ -49,183 +49,188 @@ const images = [
     "name": "Campo Claro",
     "year": 2020
   },
-  {
-    "id": 8,
-    "name": "Nuke",
-    "year": 2020
-  },
-  {
-    "id": 9,
-    "name": "Hook",
-    "year": 2020
-  },
-  {
-    "id": 10,
-    "name": "GlaubenBier",
-    "year": 2021
-  },
-  {
-    "id": 11,
-    "name": "GlaubenBier",
-    "year": 2020
-  },
-  {
-    "id": 12,
-    "name": "Hook",
-    "year": 2020
-  },
-  {
-    "id": 13,
-    "name": "Tokio",
-    "year": 2020
-  },
-  {
-    "id": 14,
-    "name": "GlaubenBier",
-    "year": 2020
-  },
-  {
-    "id": 15,
-    "name": "Juli con Essen",
-    "year": 2019
-  },
-  {
-    "id": 16,
-    "name": "Hamburgo",
-    "year": 2021
-  },
-  {
-    "id": 17,
-    "name": "Campo Claro",
-    "year": 2021
-  },
-  {
-    "id": 18,
-    "name": "Hekas",
-    "year": 2020
-  },
-  {
-    "id": 19,
-    "name": "Hamburgo",
-    "year": 2020
-  },
-  {
-    "id": 20,
-    "name": "Campo Claro",
-    "year": 2021
-  },
-  {
-    "id": 21,
-    "name": "Sustrato Growers",
-    "year": 2021
-  },
-  {
-    "id": 22,
-    "name": "Hook ",
-    "year": 2021
-  },
-  {
-    "id": 23,
-    "name": "Campo Claro",
-    "year": 2021
-  },
-  {
-    "id": 24,
-    "name": "Campo Claro",
-    "year": 2021
-  },
-  {
-    "id": "24b",
-    "name": "Hook",
-    "year": 2020
-  },
-  {
-    "id": 25,
-    "name": "Hekas",
-    "year": 2020
-  },
-  {
-    "id": 26,
-    "name": "Sustrato Growers",
-    "year": 2021
-  },
-  {
-    "id": 27,
-    "name": "Lejano Oeste",
-    "year": 2021
-  },
-  {
-    "id": 28,
-    "name": "Campo Claro",
-    "year": 2021
-  },
-  {
-    "id": 29,
-    "name": "GlaubenBier",
-    "year": 2020
-  },
-  {
-    "id": 30,
-    "name": "Hekas",
-    "year": 2020
-  },
-  {
-    "id": 31,
-    "name": "GlaubenBier",
-    "year": 2021
-  },
-  {
-    "id": 32,
-    "name": "Che!LaTorta",
-    "year": 2019
-  },
-  {
-    "id": 33,
-    "name": "Campo Claro",
-    "year": 2021
-  },
-  {
-    "id": 34,
-    "name": "Tokio",
-    "year": 2019
-  },
-  {
-    "id": 35,
-    "name": "Sushic",
-    "year": 2021
-  },
-  {
-    "id": 36,
-    "name": "Tokio",
-    "year": 2019
-  },
-  {
-    "id": 37,
-    "name": "Juli con Essen",
-    "year": 2020
-  },
-  {
-    "id": 38,
-    "name": "Che!LaTorta",
-    "year": 2019
-  },
-  {
-    "id": 39,
-    "name": "Hekas",
-    "year": 2020
-  },
-  {
-    "id": 40,
-    "name": "Hekas",
-    "year": 2020
-  }
+  // {
+  //   "id": 8,
+  //   "name": "Nuke",
+  //   "year": 2020
+  // },
+  // {
+  //   "id": 9,
+  //   "name": "Hook",
+  //   "year": 2020
+  // },
+  // {
+  //   "id": 10,
+  //   "name": "GlaubenBier",
+  //   "year": 2021
+  // },
+  // {
+  //   "id": 11,
+  //   "name": "GlaubenBier",
+  //   "year": 2020
+  // },
+  // {
+  //   "id": 12,
+  //   "name": "Hook",
+  //   "year": 2020
+  // },
+  // {
+  //   "id": 13,
+  //   "name": "Tokio",
+  //   "year": 2020
+  // },
+  // {
+  //   "id": 14,
+  //   "name": "GlaubenBier",
+  //   "year": 2020
+  // },
+  // {
+  //   "id": 15,
+  //   "name": "Juli con Essen",
+  //   "year": 2019
+  // },
+  // {
+  //   "id": 16,
+  //   "name": "Hamburgo",
+  //   "year": 2021
+  // },
+  // {
+  //   "id": 17,
+  //   "name": "Campo Claro",
+  //   "year": 2021
+  // },
+  // {
+  //   "id": 18,
+  //   "name": "Hekas",
+  //   "year": 2020
+  // },
+  // {
+  //   "id": 19,
+  //   "name": "Hamburgo",
+  //   "year": 2020
+  // },
+  // {
+  //   "id": 20,
+  //   "name": "Campo Claro",
+  //   "year": 2021
+  // },
+  // {
+  //   "id": 21,
+  //   "name": "Sustrato Growers",
+  //   "year": 2021
+  // },
+  // {
+  //   "id": 22,
+  //   "name": "Hook ",
+  //   "year": 2021
+  // },
+  // {
+  //   "id": 23,
+  //   "name": "Campo Claro",
+  //   "year": 2021
+  // },
+  // {
+  //   "id": 24,
+  //   "name": "Campo Claro",
+  //   "year": 2021
+  // },
+  // {
+  //   "id": "24b",
+  //   "name": "Hook",
+  //   "year": 2020
+  // },
+  // {
+  //   "id": 25,
+  //   "name": "Hekas",
+  //   "year": 2020
+  // },
+  // {
+  //   "id": 26,
+  //   "name": "Sustrato Growers",
+  //   "year": 2021
+  // },
+  // {
+  //   "id": 27,
+  //   "name": "Lejano Oeste",
+  //   "year": 2021
+  // },
+  // {
+  //   "id": 28,
+  //   "name": "Campo Claro",
+  //   "year": 2021
+  // },
+  // {
+  //   "id": 29,
+  //   "name": "GlaubenBier",
+  //   "year": 2020
+  // },
+  // {
+  //   "id": 30,
+  //   "name": "Hekas",
+  //   "year": 2020
+  // },
+  // {
+  //   "id": 31,
+  //   "name": "GlaubenBier",
+  //   "year": 2021
+  // },
+  // {
+  //   "id": 32,
+  //   "name": "Che!LaTorta",
+  //   "year": 2019
+  // },
+  // {
+  //   "id": 33,
+  //   "name": "Campo Claro",
+  //   "year": 2021
+  // },
+  // {
+  //   "id": 34,
+  //   "name": "Tokio",
+  //   "year": 2019
+  // },
+  // {
+  //   "id": 35,
+  //   "name": "Sushic",
+  //   "year": 2021
+  // },
+  // {
+  //   "id": 36,
+  //   "name": "Tokio",
+  //   "year": 2019
+  // },
+  // {
+  //   "id": 37,
+  //   "name": "Juli con Essen",
+  //   "year": 2020
+  // },
+  // {
+  //   "id": 38,
+  //   "name": "Che!LaTorta",
+  //   "year": 2019
+  // },
+  // {
+  //   "id": 39,
+  //   "name": "Hekas",
+  //   "year": 2020
+  // },
+  // {
+  //   "id": 40,
+  //   "name": "Hekas",
+  //   "year": 2020
+  // }
 ]
 const swipperImages = document.querySelector(".swiper-wrapper")
 for ( image of images) {
     swipperImages.innerHTML += 
     `
     <div class="slider__card-container swiper-slide">
-    <img class="slider__img" src="./assets/gallery/${image.id}.png" alt=""> 
+    <picture>
+    <source srcset="assets/gallery/${image.id}-small.jpg" media="(max-width:  480px)" />
+    <source srcset="assets/gallery/${image.id}-medium.jpg" media="(max-width: 640px)" />
+    <source srcset="assets/gallery/${image.id}-big.jpg" media="(max-width: 1200px)" />
+    <img class="slider__img" src="./assets/gallery/${image.id}.jpg" alt="gallery-ac">
+    </picture>
     <div class="slider__info-container">
         <img class="slider__info-icon" src="./assets/info.svg" alt="info logo">
         <div class="slider__info-fill">
